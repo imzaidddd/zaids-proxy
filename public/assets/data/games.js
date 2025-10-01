b15cb0eb0ed516ea988173f11fe0d9fc5d82a995
imzaidddd <zaidsafwah@gmail.com>
2025-10-01 09:47:00 -0400

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -10,6 +10,12 @@ var games = [
     url: 'https://assets.3kh0.net/idle-breakout/index.html/',
     image: 'https://play-lh.googleusercontent.com/e5jzYTn7Mjh9Ghn-DFpVedkOlRfSOyCS2AFgXdTrADIuuUbjx6vP6r_YOM2j5m7uLQ'
   },
+    {
+    id: 'testgame',
+    title: 'test',
+    url: 'https://assets.3kh0.net/idle-breakout/index.html/',
+    image: 'https://play-lh.googleusercontent.com/e5jzYTn7Mjh9Ghn-DFpVedkOlRfSOyCS2AFgXdTrADIuuUbjx6vP6r_YOM2j5m7uLQ'
+  },
   {
     id: 'eggycar',
     title: 'Eggy Car',
